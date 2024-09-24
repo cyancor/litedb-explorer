c377d641cb4a493d6c1fe9e335756bfac39b3264
Julian Baumann <git@julba.de>
2024-09-24 20:04:10 +0200

    Removed dead code

--- Sources/LiteDB.Explorer/ViewModels/MainWindowViewModel.cs
+++ Sources/LiteDB.Explorer/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
-using Avalonia.Threading;
 using LiteDB.Async;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
